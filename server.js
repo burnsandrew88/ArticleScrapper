f919043686f1c06947461c7dc4e05435f6584417
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2019-02-01 09:24:33 -0600

    starting the app

---
+++ server.js
@@ -0,0 +1,7 @@
+// npm packages
+var express = require("express");
+var exphbrs = require("express-handlebars");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require("axios");
+
